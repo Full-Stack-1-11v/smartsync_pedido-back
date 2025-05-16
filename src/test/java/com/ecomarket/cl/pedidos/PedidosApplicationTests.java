779fa478e4d24ef1f9233b4ab05606fe1271c1f7
okedux <be.ojedao@duocuc.cl>
2025-05-15 20:49:45 -0400

    api pedidos v0.1

---
+++ src/test/java/com/ecomarket/cl/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarket.cl.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
