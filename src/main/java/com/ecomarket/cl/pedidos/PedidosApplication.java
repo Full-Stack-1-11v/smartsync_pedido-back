93dd50bf80ff34921752bb1a088a6aa6714fc831
cooper8k <ange.prado@duocuc.cl>
2025-05-24 16:47:19 -0400

    1.2 agregado docker

--- src/main/java/com/ecomarket/cl/pedidos/PedidosApplication.java
+++ src/main/java/com/ecomarket/cl/pedidos/PedidosApplication.java
@@ -2,8 +2,11 @@ package com.ecomarket.cl.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class PedidosApplication {
 
 	public static void main(String[] args) {
